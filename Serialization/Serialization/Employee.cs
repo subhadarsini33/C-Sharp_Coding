5c457d79f03309df24d40ce46de870c273360f5f
subhadarsini33 <85007766+subhadarsini33@users.noreply.github.com>
2024-01-26 09:46:39 +0000

    Add files via upload

---
+++ Serialization/Serialization/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+   public  class Employee
+    {
+        public int id;
+        public string name;
+
+        public Employee(int Id, String Name)
+        {
+            this.id = Id;
+            this.name = Name;
+
+
+        }
+    }
+}
---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+     public static void Main(string[]args)
+    {
+        string path = @"D:\\s-s\sample.txt";
+        Employee emp =new  Employee(241,"subha");
+        FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(stream, emp);
+        stream.Close();
+        Console.WriteLine("File Created Successfully->" + path);
+        Console.ReadLine();
+
+    }
+
+}
